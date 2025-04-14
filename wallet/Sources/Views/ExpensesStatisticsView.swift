6e1fb50dff300f5ce84ed6a3bbd68f82092c4f57
Artem.Zaitsev <artem.zaytsev@onetwotrip.com>
2025-04-14 11:56:01 +0200

    swiftui charts

--- wallet/Sources/Views/ExpensesStatisticsView.swift
+++ wallet/Sources/Views/ExpensesStatisticsView.swift
@@ -13,7 +13,8 @@ struct ExpensesStatisticsView: View {
     @State var circleItems: [CircleItemInfo] = []
     @State var period: Period = .month
     var hasTransactions: Bool {
-        circleItems.count > 0 ? true : false
+        guard circleItems.count > 0, circleItems.first?.expenses != 0 else { return false }
+        return true
     }
     var total: Double {
         circleItems.reduce(0) { $0 + $1.expenses }
@@ -24,62 +25,65 @@ struct ExpensesStatisticsView: View {
     }
     
     var body: some View {
-        VStack {
-            ZStack {
-                ProgressCircle(items: circleItems)
-                    .padding(.horizontal, 64)
-                    .padding(.vertical, ProgressCircle.Constants.lineWidth + 12)
-                    .aspectRatio(1.0, contentMode: .fit)
-                VStack {
-                    Text("Total:")
-                        .foregroundStyle(.secondary)
-                    Text((CurrencyFormatter.formatter.string(from: .init(value: total)) ?? "") + " " + store.state.selectedCurrency.fixedSymbol)
-                        .font(.system(size: 24))
+        GeometryReader { proxy in
+            VStack(alignment: .center) {
+                let s = abs(proxy.size.width - 48)
+                ZStack {
+                    PieChartsView(data: circleItems)
+                    VStack {
+                        Text("Total:")
+                            .foregroundStyle(.secondary)
+                        Text((CurrencyFormatter.formatter.string(from: .init(value: total)) ?? "") + " " + store.state.selectedCurrency.fixedSymbol)
+                            .font(.system(size: 24))
+                    }
                 }
-            }
-            Picker("Period", selection: $period) {
-                ForEach(Period.allCases, id: \.self) { period in
-                    Text(period.representation)
+                .frame(width: s, height: s)
+                .padding(.bottom, 16)
+                
+                Picker("Period", selection: $period) {
+                    ForEach(Period.allCases, id: \.self) { period in
+                        Text(period.representation)
+                    }
                 }
-            }
-            .pickerStyle(.segmented)
-            if hasTransactions {
-                List {
-                    Grid {
-                        GridRow {
-                            Text("Category")
-                            Text("%")
-                            Text("Total")
-                        }
-                        Divider()
-                        ForEach(circleItems) { item in
+                .pickerStyle(.segmented)
+                if hasTransactions {
+                    List {
+                        Grid {
                             GridRow {
-                                Text(LocalizedStringKey(item.name))
-                                Text((CurrencyFormatter.formatter.string(from: .init(value: item.percent * 100)) ?? "") + "%")
-                                Text((CurrencyFormatter.formatter.string(from: .init(value: item.expenses)) ?? "") + " " + item.currency.fixedSymbol)
+                                Text("Category")
+                                Text("%")
+                                Text("Total")
                             }
-                            .background(item.color)
-                            .foregroundStyle(.white)
-                            
-                            if item != circleItems.last {
-                                Divider()
+                            Divider()
+                            ForEach(circleItems) { item in
+                                GridRow {
+                                    Text(LocalizedStringKey(item.name))
+                                    Text((CurrencyFormatter.formatter.string(from: .init(value: item.percent * 100)) ?? "") + "%")
+                                    Text((CurrencyFormatter.formatter.string(from: .init(value: item.expenses)) ?? "") + " " + item.currency.fixedSymbol)
+                                }
+                                .background(item.color)
+                                .foregroundStyle(.white)
+                                
+                                if item != circleItems.last {
+                                    Divider()
+                                }
                             }
                         }
                     }
+                    Spacer()
+                } else {
+                    Spacer()
+                    Text("Statistics.Zeroscreen.description")
+                        .font(.system(size: 32))
+                        .multilineTextAlignment(.center)
+                    Spacer()
                 }
-                Spacer()
-            } else {
-                Spacer()
-                Text("Statistics.Zeroscreen.description")
-                    .font(.system(size: 32))
-                    .multilineTextAlignment(.center)
-                Spacer()
             }
-        }
-        .padding()
-        .navigationTitle("Expenses")
-        .onChange(of: period, initial: true) { _, newValue in
-            self.circleItems = calculateCircleItems(store.state.transactions, expenses: store.state.expenses, period: newValue, rates: store.state.rates, currency: store.state.selectedCurrency)
+            .padding()
+            .navigationTitle("Expenses")
+            .onChange(of: period, initial: true) { _, newValue in
+                self.circleItems = calculateCircleItems(store.state.transactions, expenses: store.state.expenses, period: newValue, rates: store.state.rates, currency: store.state.selectedCurrency)
+            }
         }
     }
     
@@ -113,6 +117,10 @@ struct ExpensesStatisticsView: View {
                                   percent: item.value / overallExpenses,
                                   currency: currency,
                                   color: CircleItemInfo.preferredColors[safe: index] ?? .yellow)
+        }
+        
+        guard items.count > 0 else {
+            return [.init(name: "", icon: "", expenses: 0.0, percent: 1.0, currency: .USD, color: .blue)]
         }
         
         return items
