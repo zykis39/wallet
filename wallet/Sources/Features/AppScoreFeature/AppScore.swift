cdc78a5be7b57748d4b5c6f63b5b90672b1a63f8
Artem.Zaitsev <artem.zaytsev@onetwotrip.com>
2025-05-25 06:28:59 +0200

    score textfield fixes

--- wallet/Sources/Features/AppScoreFeature/AppScore.swift
+++ wallet/Sources/Features/AppScoreFeature/AppScore.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct AppScore: View {
     @Bindable var store: StoreOf<AppScoreFeature>
+    @FocusState var focused: Bool
     
     var body: some View {
         VStack(spacing: 24) {
@@ -18,12 +19,15 @@ struct AppScore: View {
             RatingView(rating: $store.score.sending(\.scoreChanged))
             TextEditor(text: $store.review.sending(\.reviewChanged))
                 .frame(maxHeight: 120)
+                .focused($focused)
+                .autocorrectionDisabled()
                 .overlay(
                     RoundedRectangle(cornerRadius: 10)
                         .stroke(.primary, lineWidth: 2 / 3)
                         .fill(Color.secondary.gradient.opacity(0.075))
                         .opacity(0.3)
                 )
+            
             Button { [weak store] in
                 guard let store else { return }
                 store.send(.sendReview(store.state.score, store.state.review))
@@ -31,6 +35,17 @@ struct AppScore: View {
                 Text("Send")
             }.buttonStyle(.bordered)
             Spacer()
+        }
+        .onAppear {
+            focused = true
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                Button("Done") {
+                    focused = false
+                }
+            }
         }
         .padding()
     }
--- wallet/Sources/Features/TransactionsListFeature/TransactionsListView.swift
+++ wallet/Sources/Features/TransactionsListFeature/TransactionsListView.swift
@@ -10,51 +10,10 @@ import SwiftUI
 
 struct TransactionsListView: View {
     @Bindable var store: StoreOf<TransactionsListFeature>
-//    var todayTransactions: [WalletTransaction] {
-//        store.state.transactions
-//            .filter { $0.timestamp.isEqual(to: .now, toGranularity: .day) }
-//            .sorted(by: { $0.timestamp > $1.timestamp })
-//    }
-//    var yesterdayTransactions: [WalletTransaction] {
-//        store.state.transactions
-//            .filter { t in
-//                Calendar.current.isDateInYesterday(t.timestamp) &&
-//                !todayTransactions.contains(where: { today in today.id == t.id })
-//            }
-//            .sorted(by: { $0.timestamp > $1.timestamp })
-//    }
-//    var weekTransactions: [WalletTransaction] {
-//        store.state.transactions.filter { $0.timestamp.isEqual(to: .now, toGranularity: .weekOfYear) }
-//            .filter { t in
-//                !todayTransactions.contains(where: { today in today.id == t.id }) &&
-//                !yesterdayTransactions.contains(where: { today in today.id == t.id })
-//            }
-//            .sorted(by: { $0.timestamp > $1.timestamp })
-//    }
-//    var monthTransactions: [WalletTransaction] {
-//        store.state.transactions.filter { $0.timestamp.isEqual(to: .now, toGranularity: .month) }
-//            .filter { t in
-//                !todayTransactions.contains(where: { today in today.id == t.id }) &&
-//                !yesterdayTransactions.contains(where: { today in today.id == t.id }) &&
-//                !weekTransactions.contains(where: { today in today.id == t.id })
-//            }
-//            .sorted(by: { $0.timestamp > $1.timestamp })
-//    }
-//    var allTransactions: [WalletTransaction] {
-//        store.state.transactions.filter { $0.timestamp.isEqual(to: .now, toGranularity: .month) }
-//            .filter { t in
-//                !todayTransactions.contains(where: { today in today.id == t.id }) &&
-//                !yesterdayTransactions.contains(where: { today in today.id == t.id }) &&
-//                !weekTransactions.contains(where: { today in today.id == t.id }) &&
-//                !monthTransactions.contains(where: { today in today.id == t.id })
-//            }
-//            .sorted(by: { $0.timestamp > $1.timestamp })
-//    }
     
     var body: some View {
         VStack {
             List {
-//                let items = [store.state.accounts + store.state.expenses].flatMap { $0 }
                 let items = store.state.items
                 let currencies = store.state.currencies
                 TSection(name: "Today",
